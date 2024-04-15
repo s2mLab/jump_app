6ab9a13c4cb9ae8f934516e4d8d128f97ddd251f
Pariterre <pariterre@hotmail.com>
2024-04-15 11:20:37 -0400

    Fixed overflow

--- lib/widgets/helpers.dart
+++ lib/widgets/helpers.dart
@@ -63,31 +63,33 @@ void showModel(BuildContext context,
     builder: (BuildContext context) {
       return AlertDialog(
         title: title != null ? Text(title) : null,
-        content: Column(
-          mainAxisSize:
-              MainAxisSize.min, // Use this to constrain the dialog size
-          children: <Widget>[
-            /*if (content != null) Text(content),
-            if (imageName != null)
-              Container(
-                constraints: BoxConstraints(
-                  maxWidth: MediaQuery.of(context).size.width *
-                      0.95, // 70% of screen width
-                  maxHeight: MediaQuery.of(context).size.height *
-                      0.95, // 70% of screen height
+        content: SingleChildScrollView(
+          child: Column(
+            mainAxisSize:
+                MainAxisSize.min, // Use this to constrain the dialog size
+            children: <Widget>[
+              /*if (content != null) Text(content),
+              if (imageName != null)
+                Container(
+                  constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width *
+                        0.95, // 70% of screen width
+                    maxHeight: MediaQuery.of(context).size.height *
+                        0.95, // 70% of screen height
+                  ),
+                  child: AspectRatio(
+                    aspectRatio: 16 /
+                        8, // change this value as per your image aspect ratio
+                    child: Image.asset(
+                        imageName, //'assets/images/modele_biomeca.png',
+                        fit: BoxFit.cover),*/
+              if (imageName != null)
+                Image.asset(
+                  imageName,
+                  fit: BoxFit.cover,
                 ),
-                child: AspectRatio(
-                  aspectRatio: 16 /
-                      8, // change this value as per your image aspect ratio
-                  child: Image.asset(
-                      imageName, //'assets/images/modele_biomeca.png',
-                      fit: BoxFit.cover),*/
-            if (imageName != null)
-              Image.asset(
-                imageName,
-                fit: BoxFit.cover,
-              ),
-          ],
+            ],
+          ),
         ),
       );
     },
